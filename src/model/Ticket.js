22bbdeb0c3378031b0f300f2602f78f26bbc0d4c
Pedro Lima <pedrohblima03@gmail.com>
2022-06-26 14:22:17 -0300

    add associates in UserTickets

--- src/model/Ticket.js
+++ src/model/Ticket.js
@@ -6,57 +6,22 @@ class Ticket extends Model {
       address: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-          lenght: {
-            min: 3,
-            max: 255
-          },
-        },
       },
       date: {
         type: DataTypes.DATE,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-          lenght: {
-            min: 3,
-            max: 255
-          },
-        },
       },
       time: {
         type: DataTypes.TIME,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-          lenght: {
-            min: 3,
-            max: 255
-          },
-        },
       },
       value: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-          lenght: {
-            min: 3,
-            max: 255
-          },
-        },
       },
       description: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-          lenght: {
-            min: 3,
-            max: 255
-          },
-        },
       },
     }, {
       sequelize: connection,
--- src/model/UserTickets.js
+++ src/model/UserTickets.js
@@ -20,13 +20,22 @@ class UserTicket extends Model {
       toSell: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-        validate: {
-          notEmpty: true,
-        },
       }
     }, {
       sequelize: connection,
     });
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      onDelete: 'CASCADE',
+    });
+    this.belongsTo(models.Ticket, {
+      foreignKey: 'ticket_id',
+      onDelete: 'CASCADE',
+    });
+    
   }
 }
 
